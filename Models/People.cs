124a424ff0591f819089d5cb84d1b61d11145b3c
Osni Pezzini Junior <osni.pezzini@gmail.com>
2020-12-28 21:13:52 -0300

    Criado classe de pessoas

---
+++ Models/People.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JobTo.Common.Models
+{
+    public class People
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public DateTime? Birthday { get; set; }
+    }
+}
